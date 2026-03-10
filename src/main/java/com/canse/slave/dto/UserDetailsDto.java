c645b3a5f2bd67b264a244b1528912f8c5a818d2
canse <jucansell@gmail.com>
2026-03-10 10:52:40 +0100

    :recycle: Upgrade user info details

--- src/main/java/com/canse/slave/dto/UserDetailsDto.java
+++ src/main/java/com/canse/slave/dto/UserDetailsDto.java
@@ -2,6 +2,6 @@ package com.canse.slave.dto;
 
 import java.time.LocalDateTime;
 
-public record UserDetailsDto(Long id, String username, String email, LocalDateTime createdAt, Integer nbChallenge, Integer nbFriend) {
+public record UserDetailsDto(Long id, String username, String email, LocalDateTime createdAt,LocalDateTime updatedAt, Integer nbChallenge, Integer nbFriend) {
 }
 
--- src/main/java/com/canse/slave/services/UserServiceImpl.java
+++ src/main/java/com/canse/slave/services/UserServiceImpl.java
@@ -16,7 +16,6 @@ import com.canse.slave.utils.EmailSender;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -163,7 +162,7 @@ public class UserServiceImpl implements UserService {
         Users u = userRepository.findByUsername(username);
         Integer nbChallenge = challengeService.getChallenges(username).size();
         Integer nbFriend = friendshipService.getFriends(username).size();
-        return new UserDetailsDto(u.getId(), u.getUsername(), u.getEmail(), u.getCreatedAt(), nbChallenge, nbFriend);
+        return new UserDetailsDto(u.getId(), u.getUsername(), u.getEmail(), u.getCreatedAt(), u.getUpdatedAt(), nbChallenge, nbFriend);
     }
 
     @Override
